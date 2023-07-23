17461a3460107d634629d859a78229e3c73542f0
IvanNasonov <jessica_pes@mail.ru>
2023-07-23 19:14:33 +0600

    refactor(sequence): rewrite using FP

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -1,3 +1,25 @@
+import {
+  prop,
+  compose,
+  length,
+  trim,
+  gte,
+  __,
+  allPass,
+  lte,
+  lt,
+  test,
+  ifElse,
+  tap,
+  partial,
+  assoc,
+  andThen,
+  otherwise,
+  modulo,
+  curry,
+} from "ramda";
+import Api from "../tools/api";
+
 /**
  * @file Домашка по FP ч. 2
  *
@@ -14,38 +36,88 @@
  * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
  * Ответ будет приходить в поле {result}
  */
- import Api from '../tools/api';
 
- const api = new Api();
+// utils
+const getNumberLength = compose(length, String);
+const thenGetNumberLength = andThen(getNumberLength);
+const square = (n) => n ** 2;
+const thenSquare = andThen(square);
+const thenGetThreeModulo = andThen(modulo(__, 3));
 
- /**
-  * Я – пример, удали меня
-  */
- const wait = time => new Promise(resolve => {
-     setTimeout(resolve, time);
- })
+const api = new Api();
 
- const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-     /**
-      * Я – пример, удали меня
-      */
-     writeLog(value);
+// api requests
+const getResult = prop("result");
+const thenGetResult = andThen(getResult);
 
-     api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-         writeLog(result);
-     });
+const convertUrl = "https://api.tech/numbers/base";
+const addNumberToParams = assoc("number", __, { from: 10, to: 2 });
 
-     wait(2500).then(() => {
-         writeLog('SecondLog')
+const animalsUrl = "https://animals.tech/";
+const getAnimal = curry((__) => api.get(animalsUrl + __, {}));
+const thenGetAnimal = andThen(getAnimal);
 
-         return wait(1500);
-     }).then(() => {
-         writeLog('ThirdLog');
+const getBinaryNumber = compose(
+  thenGetResult,
+  api.get(convertUrl),
+  addNumberToParams
+);
 
-         return wait(400);
-     }).then(() => {
-         handleSuccess('Done');
-     });
- }
+// validating input
+const getIsCorrectLength = allPass([gte(__, 2), lte(__, 10)]);
+const isValidLength = compose(getIsCorrectLength, length);
+
+const isPositive = compose(lt(0), parseFloat);
+
+const onlyNumbersAndDotRegex = /^[0-9.]+$/g;
+const hasOnlyNumbersAndDot = test(onlyNumbersAndDotRegex);
+
+const isValidInput = allPass([isValidLength, hasOnlyNumbersAndDot, isPositive]);
+
+// converting input
+const convertInputToNumber = compose(Math.round, parseFloat);
+
+const processSequence = ({ value, writeLog, handleSuccess, handleError }) => {
+  const log = tap(writeLog);
+  const thenLog = andThen(log);
+  const catchErrors = otherwise(handleError);
+
+  const handleInvalidInput = partial(handleError, ["ValidationError"]);
+
+  const convertToNumberAndLog = compose(log, convertInputToNumber);
+
+  const convertToBinaryAndLog = compose(thenLog, getBinaryNumber);
+
+  const logNumberLength = compose(thenLog, thenGetNumberLength);
+
+  const logSquare = compose(thenLog, thenSquare);
+
+  const logThreeModulo = compose(thenLog, thenGetThreeModulo);
+
+  const getAnimal = compose(thenGetResult, thenGetAnimal);
+
+  const thenHandleSuccess = andThen(handleSuccess);
+
+  const validInputSequence = compose(
+    catchErrors,
+    thenHandleSuccess,
+    getAnimal,
+    logThreeModulo,
+    logSquare,
+    logNumberLength,
+    convertToBinaryAndLog,
+    convertToNumberAndLog
+  );
+
+  const runWithValidInput = ifElse(
+    isValidInput,
+    validInputSequence,
+    handleInvalidInput
+  );
+
+  const logAndRun = compose(runWithValidInput, trim, log);
+
+  logAndRun(value);
+};
 
 export default processSequence;
